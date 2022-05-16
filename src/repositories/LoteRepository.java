66cd7fdcc9a3eb538c6a068c60961e31ab39e25e
fabiomorais <fabio.jorge@gmail.com>
2022-05-16 17:00:44 -0300

    Criando repositórios para manipular catálogos de produto e lote

---
+++ src/repositories/LoteRepository.java
@@ -0,0 +1,38 @@
+package repositories;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.Lote;
+
+public class LoteRepository {
+	
+	Map<String, Lote> lotes;
+	
+	public LoteRepository() {
+		this.lotes = new HashMap<String, Lote>();
+	}
+	
+	public Collection<Lote> getAll() {
+		return this.lotes.values();
+	}
+	
+	public Lote getLote(String id) {
+		return this.lotes.get(id);
+	}
+	
+	public void delLot(String id) {
+		this.lotes.remove(id);
+	}
+	
+	public void editLote(Lote lote) {
+		this.lotes.replace(lote.getId(), lote);
+	}
+	
+	public String addLote(Lote lote) {
+		this.lotes.put(lote.getId(), lote);
+		return lote.getId();
+	}
+
+}
